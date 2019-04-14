9a41f085179389c7608499f4922fb25bc2d772c8
RenatoFrancisco <renatofrancisco.s@gmail.com>
2019-04-14 11:44:01 -0300

    04 O padrão de projeto Module Pattern.

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,49 +1,52 @@
 
-var stores = ['negociacoes'];
-var version = 4;
-var dbName = 'aluraframe';
+var ConnectionFactory = (function() {
+    var stores = ['negociacoes'];
+    var version = 4;
+    var dbName = 'aluraframe';
+    
+    return class ConnectionFactory {
+    
+            constructor() {
+        
+                throw Error('Não é permitido criar instâncias de ConnectionFactory.')
+            }
+        
+            static getConnection() {
+        
+                return new Promise((resolve, reject) => {
+        
+                    let openRequest = window.indexedDB.open(dbName, version);
+        
+                    openRequest.onupgradeneeded = e => {
+        
+                        ConnectionFactory._createStores(e.target.result);
+                    };
+        
+                    openRequest.onsuccess = e =>  {
+        
+                        resolve(e.target.result);
+                    };
+        
+                    openRequest.onerror = e => {
+        
+                        console.log(e.target.error);
+        
+                        reject(e.target.error.name);
+                    };
+                });
+            }
+        
+            static _createStores(connection) {
+        
+                stores.forEach(store => {
+        
+                    if (connection.objectStoreNames.contains(store))
+                        connection.deleteObjectStore(store);
+                    
+                    connection.createObjectStore(store, { autoIncrement: true });
+                });
+            }
+        }
+})();
 
-class ConnectionFactory {
-
-    constructor() {
-
-        throw Error('Não é permitido criar instâncias de ConnectionFactory.')
-    }
-
-    static getConnection() {
-
-        return new Promise((resolve, reject) => {
-
-            let openRequest = window.indexedDB.open(dbName, version);
-
-            openRequest.onupgradeneeded = e => {
-
-                ConnectionFactory._createStores(e.target.result);
-            };
-
-            openRequest.onsuccess = e =>  {
-
-                resolve(e.target.result);
-            };
-
-            openRequest.onerror = e => {
-
-                console.log(e.target.error);
-
-                reject(e.target.error.name);
-            };
-        });
-    }
-
-    static _createStores(connection) {
-
-        stores.forEach(store => {
-
-            if (connection.objectStoreNames.contains(store))
-                connection.deleteObjectStore(store);
-            
-            connection.createObjectStore(store, { autoIncrement: true });
-        });
-    }
-}
 
