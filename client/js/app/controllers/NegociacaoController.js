ae723095067882e1c8559a3719b2faea15e9862a
RenatoFrancisco <renatofrancisco.s@gmail.com>
2019-04-19 00:55:53 -0300

    04 Importando negociações automaticamente.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -17,18 +17,28 @@ class NegociacaoController {
             new Mensagem(), new MensagemView($('#mensagemView')),
             'texto');
             
-        ConnectionFactory
-            .getConnection()
-            .then(connection => new NegociacaoDao(connection))
-            .then(dao => dao.listaTodos())
-            .then(negociacoes => 
-                negociacoes.forEach(negociacao =>
-                    this._listaNegociacoes.adiciona(negociacao)
-                )
-            )
-            .catch(erro => this._mensagem.texto = erro);
+        this._init();
     }
     
+    _init() {
+
+        ConnectionFactory
+        .getConnection()
+        .then(connection => new NegociacaoDao(connection))
+        .then(dao => dao.listaTodos())
+        .then(negociacoes => 
+            negociacoes.forEach(negociacao =>
+                this._listaNegociacoes.adiciona(negociacao)
+            )
+        )
+        .catch(erro => this._mensagem.texto = erro);
+
+        setInterval(() => {
+
+            this.importaNegociacoes();
+        }, 3000);
+    }
+
     adiciona(event) {
 
         event.preventDefault();
